1fb0914226a583ca6e1ab5ab2264a9be7c1f3ef2
Natalie Cavallo <nc_603@usc.edu>
2024-07-17 20:34:25 -0700

    added photos

--- generate_new_heatmap.py
+++ generate_new_heatmap.py
@@ -5,7 +5,7 @@ def main():
 
     plotter = Plotter(['0','1','2','3'])
     tool = GPRegressor(0.1,0.2, 4)
-    plotter.plot_heatmap('2024-6-18_Mh24_Loc1_Path1_10_12_am_Trial3.csv',True, gpregressor=tool, match_scale= True, normalize=True)
+    plotter.plot_heatmap('2024-06-19x005_Mh24_Loc2_Path4_12_3flagd1.csv',True, gpregressor=tool, match_scale= True, normalize=True)
 
 main()
 
