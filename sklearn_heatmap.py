b9826c8a145ca669c7858b05353772b767a3c8d8
ngcavallo4 <144477843+ngcavallo4@users.noreply.github.com>
2024-07-24 23:37:40 -0700

    Add files via upload

--- sklearn_heatmap.py
+++ sklearn_heatmap.py
@@ -7,18 +7,24 @@ import time
 
 def main():
 
+    # small_scale = 0.03/1111110 # deg/m * m = deg 
+    # med_scale = 0.1/1111110 # deg/m * m = deg
+    # long_scale = 0.5/1111110 # deg/m * m = deg 
+
+    small_scale = 0.03
+    med_scale = 0.1
+    long_scale = 0.5
+    
     # To plot multiple legs, pass in all the legs you want to plot into the list leg_list. 
-    len_scale = {'0':0.0002,'2':0.0002,'0,2':0.0002}
+    len_scale = {'0': long_scale,'2': long_scale,'0,2': long_scale,'0_long': long_scale,'2_long': long_scale,'0,2_long': long_scale}
+    len_scale_bounds = {'0': (long_scale/10, long_scale*10), '2': (long_scale/10, long_scale*10),'0,2': (long_scale/10, long_scale*10)}
     plotter = Plotter(['0','2'])
-    tool = GPRegressor(len_scale,0.2,4)
+    tool = GPRegressor(len_scale,0.2,2, 2.5, len_scale_bounds)
     plotter.plot_heatmap('data/combined-2024-06-19x005_Mh24_Loc2.csv',True, gpregressor=tool,
-                    transparent = {'var %': 0.55, 'transparency': 0.35}, match_scale=True, normalize=False)
-    # {'var %': 0.55, 'transparency': 0.35}
+                    transparent = None, match_scale=True, normalize=False, optimizer = False)
+    {'var %': 0.55, 'transparency': 0.4}
+
 start_time = time.time()  
 main()
 end_time = time.time()
 print(f"Elapsed time {end_time - start_time} seconds to calculate & plot")
-
-0.000005
-0.00001
-0.0002
