e74dbd47ad6b01db925181c6d6ec25238772f651
ngcavallo4 <144477843+ngcavallo4@users.noreply.github.com>
2024-07-24 23:38:01 -0700

    Add files via upload

--- tools/gpregressor.py
+++ tools/gpregressor.py
@@ -1,46 +1,66 @@
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, WhiteKernel, Matern, ConstantKernel as C
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel, Matern, RationalQuadratic, ConstantKernel as C
 import numpy as np
 import time
 
 class GPRegressor():
     
-    def __init__(self, length_scale: dict, noise_level: float, sigma_f: float):
+    def __init__(self, length_scale: dict, noise_level: float, sigma_f: float, alpha: float, len_scale_bounds: dict):
         self.length_scale = length_scale 
         self.noise_level = noise_level
         self.sigma_f = sigma_f
+        self.alpha = alpha
+        self.length_scale_bounds = len_scale_bounds
 
     def create_kernel(self, request):
         # length scale: controls the smoothness of the function
         # noise_level – Noise level
         # sigma_f – Signal variance
 
+        request_long = str(request + "_long")
+
+        len_scale_bounds = self.length_scale_bounds[request]
+
         len_scale = self.length_scale[request]
+        len_scale_long = self.length_scale[request_long]
 
         # Define the kernel components
-        kernel = C(self.sigma_f**2, (1e-3, 1e3)) * Matern(length_scale=len_scale, length_scale_bounds=(1e-5,1e5), nu=1.5) + WhiteKernel(self.noise_level, noise_level_bounds=(1e-5,1))
+        kernel = (C(self.sigma_f**2, (1e-3, 1e3)) 
+                * Matern(length_scale=len_scale, length_scale_bounds=len_scale_bounds, nu=1.0) 
+                + WhiteKernel(self.noise_level, noise_level_bounds=(1e-3,1e2)))
 
+        # kernel = (C(self.sigma_f**2, (1e-3, 1e3))
+        #         * 0.5 * Matern(length_scale=len_scale, length_scale_bounds=(1e-5,1e5), nu=1.5)
+        #         + RBF(length_scale=len_scale_long,length_scale_bounds=(1e-2,1e2))
+        #         + WhiteKernel(self.noise_level, noise_level_bounds=(1e-5,1)))
+        
+        # kernel = (C(self.sigma_f**2, (1e-3, 1e3)) 
+        #     * RationalQuadratic(length_scale=len_scale_long,length_scale_bounds=(1e-5,1e5), alpha = self.alpha, alpha_bounds = (1e-5,1e5))
+        #     * Matern(length_scale=len_scale, length_scale_bounds=(1e-5,1e5), nu=1.5))
+        
         return kernel
 
-    def Gaussian_Estimation(self, data, grid, prediction_range,  optimizer, kernel = None):
+    def Gaussian_Estimation(self, data, values, prediction_range,  optimizer: bool, kernel = None):
 
     # Instantiate the Gaussian Process Regressor
         if (not optimizer):
-            gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=15, random_state=0, optimizer=None)
+            gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=15, random_state=0)
         else:
             kernel = C(10)*RBF(1.0)
             gp = GaussianProcessRegressor(kernel, n_restarts_optimizer=15)
         
         # Fit the model to the data
-        gp.fit(data, grid)
+        gp.fit(data, values)
 
         # Make predictions on new data points
-        data_new = prediction_range
+        data_new = prediction_range.T
         z_pred, z_std = gp.predict(data_new, return_std=True)
         
         #calculate discrepancy
         information = np.exp(-np.square(z_std))
+        kernel = gp.kernel_
+        params = kernel.get_params()
         
         # noise_level_optimized = gp.kernel_.get_params()["k2__noise_level"]
     
-        return z_pred, z_std, information #, noise_level_optimized
+        return z_pred, z_std, params #, noise_level_optimized
--- tools/plotter.py
+++ tools/plotter.py
@@ -3,6 +3,7 @@ import numpy as np
 from utility.parse_csv import CSVParser
 from tools.gpregressor import GPRegressor
 from matplotlib import ticker
+from utility.convert_gps import gps_to_meters
 
 class Plotter():
      
@@ -77,25 +78,43 @@ class Plotter():
     def perform_kriging(self, gpregressor, x, y, stiff, x_range, y_range, optimizer, request):
         estimated_num = 100
         xx1, xx2 = np.linspace(x_range[0], x_range[1], num=estimated_num), np.linspace(y_range[0], y_range[1], num=estimated_num)
-        vals = np.array([[x1_, x2_] for x1_ in xx1 for x2_ in xx2])
+        vals = np.array([[x1_, x2_] for x1_ in xx1 for x2_ in xx2]).T
 
         robot_measured_points = np.vstack((x, y)).T
 
         kernel = gpregressor.create_kernel(request)
-        z_pred, z_std, information_shear = gpregressor.Gaussian_Estimation(robot_measured_points, stiff, vals, optimizer, kernel=kernel)
+        z_pred, z_std, params = gpregressor.Gaussian_Estimation(robot_measured_points, stiff, vals, optimizer, kernel=kernel)
         z_pred = z_pred.reshape(estimated_num, estimated_num).T
         z_std = z_std.reshape(estimated_num, estimated_num).T
 
+        print(f"\nLeg {request} params: {params}\n")
+
         var = np.square(z_std)
 
-        return z_pred, var
-    
-    def plot_field(self, ax, field, x_range, y_range, alpha, match_scale, colormin, colormax, title, x, y, stiff, field_name):
+        z_pred[z_pred < 0] = 0
 
-        # if field_name == "Interpolation":
-        im = ax.imshow(field, origin='lower', cmap='viridis', extent=(x_range[0], x_range[1], y_range[0], y_range[1]), alpha=alpha)
-        # else:
-        #     im = ax.imshow(field, origin='lower', cmap='viridis', extent=(x_range[0], x_range[1], y_range[0], y_range[1]))
+        return z_pred, var
+
+    def plot_leg(self, axis_index, z_pred, var, x, y, stiff, x_range, y_range, title, match_scale, zmin, zmax, var_min, var_max, transparent: dict=None):
+
+        z_alpha = np.ones_like(z_pred)
+        if transparent is not None:
+            var_percent = transparent['var %']
+            transparency = transparent['transparency']
+            transparency_var = var_percent*var_max 
+            z_alpha[var > transparency_var] = 1 - transparency
+
+        fields = [('Interpolation', z_pred, zmin, zmax, "Stiffness (N/m)"), ('Variance', var, var_min, var_max, "(N/m)^2")]
+        for i, (field_name, field, fmin, fmax, field_unit) in enumerate(fields):
+            ax_field = self.axs[i, axis_index] if len(self.mode) > 1 else self.axs[i]
+            self.plot_field(ax_field, field, x_range, y_range, z_alpha, match_scale, fmin, fmax, title, x, y, stiff, field_name, field_unit)
+    
+    def plot_field(self, ax, field, x_range, y_range, alpha, match_scale, colormin, colormax, title, x, y, stiff, field_name, field_unit):
+
+        if field_name == "Interpolation":
+            im = ax.imshow(field, origin='lower', cmap='viridis', extent=(x_range[0], x_range[1], y_range[0], y_range[1]), alpha=alpha)
+        else:
+            im = ax.imshow(field, origin='lower', cmap='viridis', extent=(x_range[0], x_range[1], y_range[0], y_range[1]))
         ax.set_xlim([x_range[0], x_range[1]])
         ax.set_ylim([y_range[0], y_range[1]])
         if match_scale:
@@ -109,30 +128,16 @@ class Plotter():
         ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
         cbar = self.fig.colorbar(im, ax=ax, shrink=0.7)
         cbar.ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+        cbar.set_label(f'{field_unit}', rotation=270, labelpad = 10)
 
         # Set x-axis label
         offset_text = ax.xaxis.get_offset_text()
         offset_text.set_size(7)
         offset_text = ax.yaxis.get_offset_text()
         offset_text.set_size(7)
-        ax.set_xlabel('Latitude(º)',fontsize=10)
-        ax.xaxis.set_label_coords(0.5, -0.16)
-        ax.set_ylabel("Longitude(º)",loc='center',fontsize=10)
-
-
-    def plot_leg(self, axis_index, z_pred, var, x, y, stiff, x_range, y_range, title, match_scale, zmin, zmax, var_min, var_max, transparent: dict=None):
-
-        z_alpha = np.ones_like(z_pred)
-        if transparent is not None:
-            var_percent = transparent['var %']
-            transparency = transparent['transparency']
-            transparency_var = var_percent*var_max 
-            z_alpha[var > transparency_var] = 1 - transparency
-
-        fields = [('Interpolation', z_pred, zmin, zmax), ('Variance', var, var_min, var_max)]
-        for i, (field_name, field, fmin, fmax) in enumerate(fields):
-            ax_field = self.axs[i, axis_index] if len(self.mode) > 1 else self.axs[i]
-            self.plot_field(ax_field, field, x_range, y_range, z_alpha, match_scale, fmin, fmax, title, x, y, stiff, field_name)
+        ax.set_xlabel("Longitude(º)",fontsize=10)
+        ax.xaxis.set_label_coords(0.5, -0.19)
+        ax.set_ylabel("Latitude(º)",loc='center',fontsize=10)
         
     def initialize_subplots(self):
             r"""Sets up rows of subplots based on which legs are being plotted.
@@ -199,11 +204,11 @@ class Plotter():
                 y_range[1] = np.max(y)
                 y_range[0]= y_input_range[0]
             else:
-                x_range[0] = np.min(x) - 0.000002
-                x_range[1] = np.max(x) + 0.000002
+                x_range[0] = np.min(x) - 0.25
+                x_range[1] = np.max(x) + 0.25
 
-                y_range[1] = np.max(y) + 0.000002
-                y_range[0] = np.min(y) - 0.000002
+                y_range[1] = np.max(y) + 0.25
+                y_range[0] = np.min(y) - 0.25
 
         else: # If not match steps, then must pass in values
 
