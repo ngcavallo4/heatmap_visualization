fd17f091ca9e7fa8622575e73a2c3630f4b6a639
Natalie Cavallo <nc_603@usc.edu>
2024-08-02 13:19:35 -0700

    reconfigured venv

--- sklearn_heatmap.py
+++ sklearn_heatmap.py
@@ -19,9 +19,13 @@ def main():
     scale = longest_scale
     
     # To plot multiple legs, pass in all the legs you want to plot into the list leg_list. 
-    len_scale = {'val': scale,'bounds': (0.3, 1)}
-    sigma_f = {"val": 4, "bounds": (1e-7,1e-5)}
-    noise_level = {"val": 0.2, "bounds": (1e-3, 10)}
+    # len_scale = {'val': scale,'bounds': (0.3, 1)}
+    # sigma_f = {"val": 4, "bounds": (1e-7,1e-5)}
+    # noise_level = {"val": 0.2, "bounds": (1e-3, 10)}
+
+    len_scale = {'val': scale,'bounds': (scale/2, scale*100)}
+    sigma_f = {"val": 4, "bounds": (0.2,20)}
+    noise_level = {"val": 0.2, "bounds": (1e-3, 1e2)}
 
     tool = GPRegressor(len_scale,noise_level,sigma_f, 1.0, alpha=2.5)
 
@@ -29,8 +33,8 @@ def main():
 
     # {'var %': 0.55, 'transparency': 0.4}
     plotter = Plotter(['0','2'], rotate=None)
-    plotter.plot_heatmap(PATH, 'combined-2024-06-18_Mh24_Loc1.csv',
-                        True, gpregressor=tool, transparent = None, match_scale=True,
+    plotter.plot_heatmap(PATH, 'combined-2024-06-19_Mh24_Loc2.csv',
+                        True, gpregressor=tool, transparent = {'var %': 0.95, 'transparency': 0.5}, match_scale=True,
                         latlon = False, optimizer=True)
     # {'var %': 0.95, 'transparency': 0.5}
 start_time = time.time()  
